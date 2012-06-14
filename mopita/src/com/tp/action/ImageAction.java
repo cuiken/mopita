976163c3f79efa078138a0e22ee22c8657234673
ken <ken.cuik@gmail.com>
2012-06-14 10:47:17 +0800

    进一步优化图片缓存～暂结束

--- mopita/src/com/tp/action/ImageAction.java
+++ mopita/src/com/tp/action/ImageAction.java
@@ -5,7 +5,12 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import javax.activation.MimetypesFileTypeMap;
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.ArrayUtils;
 
 import com.opensymphony.xwork2.ActionSupport;
 import com.tp.utils.Constants;
@@ -17,6 +22,14 @@ public class ImageAction extends ActionSupport {
 	private static final long serialVersionUID = 1L;
 
 	private String path;
+
+	private static final String[] GZIP_MIME_TYPES = { "text/html", "text/xml", "text/plain", "text/css",
+			"text/javascript", "application/xml", "application/xhtml+xml", "application/x-javascript" };
+
+	private static final int GZIP_MINI_LENGTH = 512;
+
+	private MimetypesFileTypeMap mimetypesFileTypeMap;
+	public static final long ONE_YEAR_SECONDS = 60 * 60 * 24 * 365;
 
 	@Override
 	public String execute() throws Exception {
@@ -32,11 +45,20 @@ public class ImageAction extends ActionSupport {
 	}
 
 	private void responseImage(String imgURL) throws Exception {
-		HttpServletResponse response = Struts2Utils.getResponse();
-		response.setContentType("image/*");
-		Servlets.setExpiresHeader(response, 315360000L);
-		Servlets.setLastModifiedHeader(response, System.currentTimeMillis());
 		File file = new File(imgURL);
+		HttpServletResponse response = Struts2Utils.getResponse();
+		HttpServletRequest request = Struts2Utils.getRequest();
+		response.setContentType("image/*");
+		ContentInfo contentInfo = getContentInfo(file);
+		//根据Etag或ModifiedSince Header判断客户端的缓存文件是否有效, 如仍有效则设置返回码为304,直接返回.
+		if (!Servlets.checkIfModifiedSince(request, response, contentInfo.lastModified)
+				|| !Servlets.checkIfNoneMatchEtag(request, response, contentInfo.etag)) {
+			return;
+		}
+		Servlets.setExpiresHeader(response, ONE_YEAR_SECONDS);
+		Servlets.setLastModifiedHeader(response, System.currentTimeMillis());
+		Servlets.setEtag(response, contentInfo.etag);
+
 		byte[] buffer = new byte[1024];
 		InputStream is = new FileInputStream(file);
 		int len = 0;
@@ -53,4 +75,42 @@ public class ImageAction extends ActionSupport {
 		this.path = path;
 	}
 
+	@PostConstruct
+	public void init() {
+		mimetypesFileTypeMap = new MimetypesFileTypeMap();
+		mimetypesFileTypeMap.addMimeTypes("text/css css");
+	}
+
+	private ContentInfo getContentInfo(File file) {
+		ContentInfo contentInfo = new ContentInfo();
+
+		contentInfo.file = file;
+		//		contentInfo.contentPath = contentPath;
+		contentInfo.fileName = file.getName();
+		contentInfo.length = (int) file.length();
+
+		contentInfo.lastModified = file.lastModified();
+		contentInfo.etag = "W/\"" + contentInfo.lastModified + "\"";
+
+		contentInfo.mimeType = mimetypesFileTypeMap.getContentType(contentInfo.fileName);
+
+		if (contentInfo.length >= GZIP_MINI_LENGTH && ArrayUtils.contains(GZIP_MIME_TYPES, contentInfo.mimeType)) {
+			contentInfo.needGzip = true;
+		} else {
+			contentInfo.needGzip = false;
+		}
+
+		return contentInfo;
+	}
+
+	static class ContentInfo {
+		protected String contentPath;
+		protected File file;
+		protected String fileName;
+		protected int length;
+		protected String mimeType;
+		protected long lastModified;
+		protected String etag;
+		protected boolean needGzip;
+	}
 }
