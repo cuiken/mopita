1ac61fdedf8a342bbefbc23fb2fb7474fb8583e3
ken <ken.cuik@gmail.com>
2012-08-08 18:14:14 +0800

    | URLEncoder

--- mopita/src/main/java/com/tp/action/HomeAction.java
+++ mopita/src/main/java/com/tp/action/HomeAction.java
@@ -169,7 +169,8 @@ public class HomeAction extends ActionSupport {
 		httpBuffer.append("&inputPath=");
 		httpBuffer.append(URLEncoder.encode(info.getTheme().getApkPath(), "utf-8"));
 		httpBuffer.append("&title=" + URLEncoder.encode(info.getTitle(), "utf-8"));
-		httpBuffer.append("|").append(URLEncoder.encode(info.getTheme().getTitle(), "utf-8"));
+		httpBuffer.append(URLEncoder.encode("|", "utf-8")).append(
+				URLEncoder.encode(info.getTheme().getTitle(), "utf-8"));
 		httpBuffer.append("&");
 		if (downType.equals(DownloadType.MARKET.getValue())) {
 			marketDownload(fromMarket, httpBuffer.toString());
